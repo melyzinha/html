047071fb250b412a80e5db9bb3ed6bf2e62d2068
melissa satou <melissacursonovo@gmail.com>
2024-03-18 20:04:33 -0300

    hamburguer e netflix

---
+++ src/fechar_nota.py
@@ -0,0 +1,11 @@
+nota1= int (input(" Entre com nota1"))
+nota2= int (input ("Entre com nota2"))
+nota3= int (input ("Entre com nota3"))
+nota4= int (input ("Entre com nota4"))
+soma= nota1 +nota2 +nota3 +nota4 
+media= soma / 4
+print(media)
+if media>=6:
+     print(" voce acertou")
+else:
+    print("voce errou")     
